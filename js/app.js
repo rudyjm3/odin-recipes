ffd340f1bcf30f98fe3b7d2357fc0fbe924e9f6b
rudyjm3 <rudyjm3@yahoo.com>
2022-04-23 22:30:24 -0400

    Updated heade and footer placement. Improved styling.  Added google font link to all pages.

--- js/app.js
+++ js/app.js
@@ -4,14 +4,25 @@
 let siteHeader = `
 <div class="header-container">
    <div class="logo-image-wrapper">
-      <img src="images/pngwing.com.png" alt="logo image" srcset="">
+      <img src="/images/pngwing.com.png" alt="logo image" srcset="">
    </div>
    <div class="logo-text-wrapper">
       <h1>Odin Recipes</h1>
    </div>
-   <hr>
+   <!-- <hr> -->
 </div>
 `;
 
+let siteFooter = `
+<div class="footer-container">
+   <div class="project-source-wrapper">
+      <img src="/images/odin-project-logo.png" alt="" srcset="">
+   </div>
+   <div class="my-info-wrapper">
+      <p>Site coded by:<a href="http://www.rudolphmims.com"> Rudolph Mims</a></p>
+   </div>
+</div>
+`;
 document.getElementById("site-header_import").innerHTML = siteHeader;
+document.getElementById("site-footer_import").innerHTML = siteFooter;
 console.log(siteHeader);
